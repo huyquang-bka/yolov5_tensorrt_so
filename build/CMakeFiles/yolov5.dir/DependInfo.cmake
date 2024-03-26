dfc0509dbdedc7a029585ed1d7444e4f5120ebac
huyquangb-bka <huyquangbka@gmail.com>
2024-03-26 18:17:54 +0700

    ...

---
+++ build/CMakeFiles/yolov5.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/atin/huyquang/yolov5_tensorrt_so/calibrator.cpp" "/mnt/atin/huyquang/yolov5_tensorrt_so/build/CMakeFiles/yolov5.dir/calibrator.cpp.o"
+  "/mnt/atin/huyquang/yolov5_tensorrt_so/yolov5_custom.cpp" "/mnt/atin/huyquang/yolov5_tensorrt_so/build/CMakeFiles/yolov5.dir/yolov5_custom.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "API_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/cuda-11.7/include"
+  "/home/a30/TensorRT-8.6.1.6/include"
+  "../opencv_calib3d"
+  "../opencv_core"
+  "../opencv_dnn"
+  "../opencv_features2d"
+  "../opencv_flann"
+  "../opencv_gapi"
+  "../opencv_highgui"
+  "../opencv_imgcodecs"
+  "../opencv_imgproc"
+  "../opencv_ml"
+  "../opencv_objdetect"
+  "../opencv_photo"
+  "../opencv_stitching"
+  "../opencv_video"
+  "../opencv_videoio"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/atin/huyquang/yolov5_tensorrt_so/build/CMakeFiles/myplugins.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
