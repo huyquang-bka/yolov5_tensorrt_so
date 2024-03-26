4c56925bef358cc7f1aee62afdb3000731c597bd
huyquangb-bka <huyquangbka@gmail.com>
2024-03-26 18:43:57 +0700

    ...

--- yolov5_custom.cpp
+++ yolov5_custom.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstdlib>
 #include <chrono>
 #include <cmath>
 #include "cuda_utils.h"
@@ -10,16 +9,16 @@
 #include "preprocess.h"
 #include "macros.h"
 
-#define USE_FP16 true
-#define DEVICE 0
+#define USE_FP16 // set USE_INT8 or USE_FP16 or USE_FP32
+#define DEVICE 0 // GPU id
 #define BATCH_SIZE 1
-#define MAX_IMAGE_INPUT_SIZE_THRESH 9000000 // ensure it exceed the maximum size in the input images !
+#define MAX_IMAGE_INPUT_SIZE_THRESH 3000 * 3000 // ensure it exceed the maximum size in the input images !
 
-// stuff we know about the network and the input/output blobs, read from environment variable
+// stuff we know about the network and the input/output blobs
 static const int INPUT_H = 640;
 static const int INPUT_W = 640;
-static const int CLASS_NUM = 1;
-static const int OUTPUT_SIZE = 1000 * sizeof(Yolo::Detection) / sizeof(float) + 1; // we assume the yololayer outputs no more than MAX_OUTPUT_BBOX_COUNT boxes that conf >= 0.1
+static const int CLASS_NUM = 80;
+static const int OUTPUT_SIZE = Yolo::MAX_OUTPUT_BBOX_COUNT * sizeof(Yolo::Detection) / sizeof(float) + 1; // we assume the yololayer outputs no more than MAX_OUTPUT_BBOX_COUNT boxes that conf >= 0.1
 const char *INPUT_BLOB_NAME = "data";
 const char *OUTPUT_BLOB_NAME = "prob";
 static Logger gLogger;
@@ -339,7 +338,6 @@ bool parse_args(int argc, char **argv, std::string &wts, std::string &engine, bo
 
 int main(int argc, char **argv)
 {
-
     cudaSetDevice(DEVICE);
     std::string wts_name = "";
     std::string engine_name = "";
@@ -368,7 +366,132 @@ int main(int argc, char **argv)
         }
         p.write(reinterpret_cast<const char *>(modelStream->data()), modelStream->size());
         modelStream->destroy();
+        return 0;
     }
+
+    // deserialize the .engine and run inference
+    std::ifstream file(engine_name, std::ios::binary);
+    if (!file.good())
+    {
+        std::cerr << "read " << engine_name << " error!" << std::endl;
+        return -1;
+    }
+    char *trtModelStream = nullptr;
+    size_t size = 0;
+    file.seekg(0, file.end);
+    size = file.tellg();
+    file.seekg(0, file.beg);
+    trtModelStream = new char[size];
+    assert(trtModelStream);
+    file.read(trtModelStream, size);
+    file.close();
+
+    std::vector<std::string> file_names;
+    if (read_files_in_dir(img_dir.c_str(), file_names) < 0)
+    {
+        std::cerr << "read_files_in_dir failed." << std::endl;
+        return -1;
+    }
+
+    // prepare input data ---------------------------
+    static float data[BATCH_SIZE * 3 * INPUT_H * INPUT_W];
+    // for (int i = 0; i < 3 * INPUT_H * INPUT_W; i++)
+    //     data[i] = 1.0;
+    static float prob[BATCH_SIZE * OUTPUT_SIZE];
+    IRuntime *runtime = createInferRuntime(gLogger);
+    assert(runtime != nullptr);
+    ICudaEngine *engine = runtime->deserializeCudaEngine(trtModelStream, size);
+    assert(engine != nullptr);
+    IExecutionContext *context = engine->createExecutionContext();
+    assert(context != nullptr);
+    delete[] trtModelStream;
+    assert(engine->getNbBindings() == 2);
+    void *buffers[2];
+    // In order to bind the buffers, we need to know the names of the input and output tensors.
+    // Note that indices are guaranteed to be less than IEngine::getNbBindings()
+    const int inputIndex = engine->getBindingIndex(INPUT_BLOB_NAME);
+    const int outputIndex = engine->getBindingIndex(OUTPUT_BLOB_NAME);
+    assert(inputIndex == 0);
+    assert(outputIndex == 1);
+    // Create GPU buffers on device
+    CUDA_CHECK(cudaMalloc(&buffers[inputIndex], BATCH_SIZE * 3 * INPUT_H * INPUT_W * sizeof(float)));
+    CUDA_CHECK(cudaMalloc(&buffers[outputIndex], BATCH_SIZE * OUTPUT_SIZE * sizeof(float)));
+    // Create stream
+    cudaStream_t stream;
+    CUDA_CHECK(cudaStreamCreate(&stream));
+
+    int fcount = 0;
+    for (int f = 0; f < (int)file_names.size(); f++)
+    {
+        fcount++;
+        if (fcount < BATCH_SIZE && f + 1 != (int)file_names.size())
+            continue;
+        for (int b = 0; b < fcount; b++)
+        {
+            cv::Mat img = cv::imread(img_dir + "/" + file_names[f - fcount + 1 + b]);
+            if (img.empty())
+                continue;
+            cv::Mat pr_img = preprocess_img(img, INPUT_W, INPUT_H); // letterbox BGR to RGB
+            int i = 0;
+            for (int row = 0; row < INPUT_H; ++row)
+            {
+                uchar *uc_pixel = pr_img.data + row * pr_img.step;
+                for (int col = 0; col < INPUT_W; ++col)
+                {
+                    data[b * 3 * INPUT_H * INPUT_W + i] = (float)uc_pixel[2] / 255.0;
+                    data[b * 3 * INPUT_H * INPUT_W + i + INPUT_H * INPUT_W] = (float)uc_pixel[1] / 255.0;
+                    data[b * 3 * INPUT_H * INPUT_W + i + 2 * INPUT_H * INPUT_W] = (float)uc_pixel[0] / 255.0;
+                    uc_pixel += 3;
+                    ++i;
+                }
+            }
+        }
+
+        // Run inference
+        auto start = std::chrono::system_clock::now();
+        doInference(*context, stream, buffers, data, prob, BATCH_SIZE);
+        auto end = std::chrono::system_clock::now();
+        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+        std::vector<std::vector<Yolo::Detection>> batch_res(fcount);
+        for (int b = 0; b < fcount; b++)
+        {
+            auto &res = batch_res[b];
+            nms(res, &prob[b * OUTPUT_SIZE], CONF_THRESH, NMS_THRESH);
+        }
+        for (int b = 0; b < fcount; b++)
+        {
+            auto &res = batch_res[b];
+            // std::cout << res.size() << std::endl;
+            cv::Mat img = cv::imread(img_dir + "/" + file_names[f - fcount + 1 + b]);
+            for (size_t j = 0; j < res.size(); j++)
+            {
+                cv::Rect r = get_rect(img, res[j].bbox);
+                cv::rectangle(img, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
+                cv::putText(img, std::to_string((int)res[j].class_id), cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+            }
+            cv::imwrite("_" + file_names[f - fcount + 1 + b], img);
+        }
+        fcount = 0;
+    }
+
+    // Release stream and buffers
+    cudaStreamDestroy(stream);
+    CUDA_CHECK(cudaFree(buffers[inputIndex]));
+    CUDA_CHECK(cudaFree(buffers[outputIndex]));
+    // Destroy the engine
+    context->destroy();
+    engine->destroy();
+    runtime->destroy();
+
+    // Print histogram of the output distribution
+    // std::cout << "\nOutput:\n\n";
+    // for (unsigned int i = 0; i < OUTPUT_SIZE; i++)
+    //{
+    //    std::cout << prob[i] << ", ";
+    //    if (i % 10 == 0) std::cout << std::endl;
+    //}
+    // std::cout << std::endl;
+
     return 0;
 }
 
@@ -432,7 +555,7 @@ extern "C" API void *Init(char *model_path)
     return (void *)trt;
 }
 
-extern "C" API void Detect(void *h, int rows, int cols, unsigned char *src_data, float (*res_array)[6], float conf_thres, float nms_thres)
+extern "C" API void Detect(void *h, int rows, int cols, unsigned char *src_data, float (*res_array)[6], float conf_thresh, float nms_thresh)
 {
     Yolov5TRTContext *trt = (Yolov5TRTContext *)h;
     cv::Mat img = cv::Mat(rows, cols, CV_8UC3, src_data);
@@ -457,7 +580,7 @@ extern "C" API void Detect(void *h, int rows, int cols, unsigned char *src_data,
 
     std::vector<std::vector<Yolo::Detection>> batch_res(1);
     auto &res = batch_res[0];
-    nms(res, &trt->prob[0 * OUTPUT_SIZE], conf_thres, nms_thres);
+    nms(res, &trt->prob[0 * OUTPUT_SIZE], conf_thresh, nms_thresh);
     int len = res.size();
     for (size_t j = 0; j < res.size(); j++)
     {
