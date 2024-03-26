9772bc17f83520e0f5e2e551d1914f2babba7256
NamND99 <ngonam21021999@gmail.com>
2024-03-26 16:49:17 +0700

    ...

--- yolov5_custom.cpp
+++ yolov5_custom.cpp
@@ -12,12 +12,19 @@
 
 
 // Retrieve environment variables or use default values
-#define USE_FP16 (getenv("USE_FP16") ? atoi(getenv("USE_FP16")) : true)
-#define DEVICE (getenv("DEVICE") ? atoi(getenv("DEVICE")) : 0)
-#define NMS_THRESH (getenv("NMS_THRESH") ? atof(getenv("NMS_THRESH")) : 0.4)
-#define CONF_THRESH (getenv("CONF_THRESH") ? atof(getenv("CONF_THRESH")) : 0.5)
-#define BATCH_SIZE (getenv("BATCH_SIZE") ? atoi(getenv("BATCH_SIZE")) : 1)
-#define MAX_IMAGE_INPUT_SIZE_THRESH (getenv("MAX_IMAGE_INPUT_SIZE_THRESH") ? atoi(getenv("MAX_IMAGE_INPUT_SIZE_THRESH")) : 3000 * 3000)
+// #define USE_FP16 (getenv("USE_FP16") ? atoi(getenv("USE_FP16")) : true)
+// #define DEVICE (getenv("DEVICE") ? atoi(getenv("DEVICE")) : 0)
+// #define NMS_THRESH (getenv("NMS_THRESH") ? atof(getenv("NMS_THRESH")) : 0.4)
+// #define CONF_THRESH (getenv("CONF_THRESH") ? atof(getenv("CONF_THRESH")) : 0.5)
+// #define BATCH_SIZE (getenv("BATCH_SIZE") ? atoi(getenv("BATCH_SIZE")) : 1)
+// #define MAX_IMAGE_INPUT_SIZE_THRESH (getenv("MAX_IMAGE_INPUT_SIZE_THRESH") ? atoi(getenv("MAX_IMAGE_INPUT_SIZE_THRESH")) : 3000 * 3000)
+
+#define USE_FP16  // set USE_INT8 or USE_FP16 or USE_FP32
+#define DEVICE 0  // GPU id
+#define NMS_THRESH 0.4
+#define CONF_THRESH 0.5
+#define BATCH_SIZE 1
+#define MAX_IMAGE_INPUT_SIZE_THRESH 3000 * 3000
 
 int INPUT_H = getenv("INPUT_H") ? atoi(getenv("INPUT_H")) : 640;
 int INPUT_W = getenv("INPUT_W") ? atoi(getenv("INPUT_W")) : 640;
